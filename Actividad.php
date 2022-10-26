8191ec71816f5ba75f3481aea0012c310c9e2f66
C4rlostah <115100339+C4rlostah@users.noreply.github.com>
2022-10-26 11:53:22 -0500

    Update Actividad.php

--- Actividad.php
+++ Actividad.php
@@ -5,6 +5,7 @@
 	<meta http-equi="X-UA-Compatlbie" content-"IE-edge">
 	<meta name "viewport" content="width-device-width, initial-scare=1.0">
 	<title>Document</title>
+	<IMG src="https://i.pinimg.com/originals/a1/77/df/a177dfc84703c31afa0d501ccf43fe4f.gif"
 </head>
 <body>
 
