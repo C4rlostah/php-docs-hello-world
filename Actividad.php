d856517a89c61f300fe90084272c6a08ff0f94e9
C4rlostah <115100339+C4rlostah@users.noreply.github.com>
2022-10-26 11:38:59 -0500

    Add files via upload

---
+++ Actividad.php
@@ -0,0 +1,17 @@
+<|DOCTYPE html PUBLIC>
+<html lang-"en">
+<head>
+	<meta charset-"UTF-a"
+	<meta http-equi="X-UA-Compatlbie" content-"IE-edge">
+	<meta name "viewport" content="width-device-width, initial-scare=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+	echo "Hello World";
+	?>
+
+<p> hola </p>
+</body>
+</html>
