98eed67c1b63e421099929d740da3c889e5378c4
C4rlostah <115100339+C4rlostah@users.noreply.github.com>
2022-10-26 23:54:06 -0500

    Update Actividad.php

--- Actividad.php
+++ Actividad.php
@@ -5,12 +5,13 @@
 	<meta http-equi="X-UA-Compatlbie" content-"IE-edge">
 	<meta name "viewport" content="width-device-width, initial-scare=1.0">
 	<title>Document</title>
-	<IMG src="https://i.pinimg.com/originals/a1/77/df/a177dfc84703c31afa0d501ccf43fe4f.gif"
+	<IMG src="https://i.pinimg.com/originals/a1/77/df/a177dfc84703c31afa0d501ccf43fe4f.gif">
+
 </head>
 <body>
 
 <?php
-	echo "Hello World";
+	echo "Hello World";</LI>
 	?>
 
 <p> hola </p>
